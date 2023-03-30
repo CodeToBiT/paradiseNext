4c4892b2fadb1c02143b660272b57503ef8996f1
Bishan Magar <[sendtobi404@gmail.com]>
2023-03-30 13:18:23 +0545

    setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "../styles/globals.scss";
+
+import NavigationBar from "./components/header/NavigationBar";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavigationBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
